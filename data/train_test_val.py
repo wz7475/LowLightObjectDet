1f975759456a7f10598885d534024fae5631aae0
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-12-11 18:42:58 +0100

    feat (data prep): remove intermediate files of dataset loading

--- data/train_test_val.py
+++ data/train_test_val.py
@@ -13,12 +13,12 @@ with open(annotations_info_path, 'r') as f:
     annotations_info = f.readlines()
 annotations_info = [x.strip() for x in annotations_info[1:]]
 
-train_test_val_map = {
+train_val_test_map = {
     "1": "train",
-    "2": "test",
-    "3": "val"
+    "2": "val",
+    "3": "test"
 }
-for dir in train_test_val_map.values():
+for dir in train_val_test_map.values():
     os.makedirs(os.path.join(dest_dir, dir), exist_ok=True)
 
 
@@ -26,7 +26,7 @@ for annotation_info in annotations_info:
     annotation_info = annotation_info.split(delimiter)
     annotation_path = annotation_info[0]
     train_test_val_idx = annotation_info[4]
-    train_test_val = train_test_val_map[train_test_val_idx]
+    train_test_val = train_val_test_map[train_test_val_idx]
 
     img_name = os.path.basename(annotation_path).lower()
     img_src_path = os.path.join(img_dir, img_name)
--- prepare_data.sh
+++ prepare_data.sh
@@ -4,10 +4,10 @@ images_url="https://drive.usercontent.google.com/download?id=1BHmPgu8EsHoFDDkMGL
 
 cd "./data" || exit ;
 
-#wget $images_url -O "ExDark.zip" ;
-#
-#unzip "ExDark.zip" ;
-#unzip "ExDark_Annno.zip" ;
+wget $images_url -O "ExDark.zip" ;
+unzip "ExDark.zip" ;
+unzip "ExDark_Annno.zip" ;
+rm ExDark.zip ;
 
 
 # make directory for data
@@ -26,6 +26,8 @@ python move_imgs_to_flat_dir.py ./ExDark ./dataset/flat && echo "flattened imgs"
 mkdir ./dataset/flat_anno -p;
 python move_imgs_to_flat_dir.py ./ExDark_Annno ./dataset/flat_anno && echo "flattened anno";
 
+rm -rf ./ExDark_Annno ExDark;
+
 # rename labels in annotation files from ExDark names to COCO names
 mkdir ./dataset/flat_anno_coco ;
 python parse_anno_files_classes.py ./dataset/flat_anno ./dataset/flat_anno_coco && echo "anno to coco";
@@ -33,5 +35,6 @@ python parse_anno_files_classes.py ./dataset/flat_anno ./dataset/flat_anno_coco 
 # train test val
 python train_test_val.py dataset/imageclasslist.txt.coco ./dataset/split ./dataset/flat ./dataset/flat_anno_coco , && echo "train test val";
 
+rm -rf ./dataset/flat ./dataset/flat ./dataset/flat_anno ./dataset/flat_anno_coco ;
 
 cd ".." ;
