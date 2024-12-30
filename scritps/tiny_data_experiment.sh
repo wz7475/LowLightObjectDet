6e9337958f4151e07ba78fb32c2997bb0f29cb10
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-12-30 18:41:26 +0100

    feat (tiny data experiment): add script for tiny data experiment

---
+++ scritps/tiny_data_experiment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "num_samples: - mAP" > results_tiny_data.txt
+
+for i in 5 10 50 75 100 200
+do
+   output=$(python exdark/modeling/train.py \
+    "datamodule.limit_to_n_samples=$i" \
+    "datamodule.batch_size=4" \
+    "trainer.max_epochs=5") ;
+   echo "$i: $output" >> results_tiny_data.txt ;
+done
