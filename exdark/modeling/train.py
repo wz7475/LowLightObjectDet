ab8e85c87212d217d1d179cb25e4c1c1921cea7d
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-12-30 12:16:38 +0100

    feat (training models): add early stopping and test run after fitting model

--- exdark/modeling/train.py
+++ exdark/modeling/train.py
@@ -1,28 +1,29 @@
 import os
+
 import hydra
-from omegaconf import DictConfig
 import lightning as L
-import torch
 import wandb
 from dotenv import load_dotenv
-from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
+from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor, EarlyStopping
 from lightning.pytorch.loggers import WandbLogger
+from omegaconf import DictConfig
 
-from exdark.data.datamodules.exdarkdatamodule import ExDarkDataModule
 from exdark.data.datamodules.gammadatamodule import GammaBrightenExDarkDataModule
-from exdark.models.exdarkdedicatedmodels.fasterrcnn import FasterRCNN
-from exdark.models.callbacks import (
+from exdark.logging.callbacks import (
     LogDataModuleCallback,
     LogModelCallback,
     LogTransformationCallback,
 )
 
+
 def setup_environment():
     load_dotenv()
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
+
 def get_data_module(batch_size):
     return GammaBrightenExDarkDataModule(batch_size=batch_size)
+
 
 def get_callbacks():
     checkpoints = ModelCheckpoint(
@@ -31,12 +32,22 @@ def get_callbacks():
         monitor="val_mAP",
         every_n_epochs=1,
     )
+    early_stopping = EarlyStopping(monitor="val_mAP", mode="max", min_delta=0.005)
     lr_monitor = LearningRateMonitor(logging_interval="step", log_momentum=True)
-    return [checkpoints, lr_monitor, LogDataModuleCallback(), LogModelCallback(), LogTransformationCallback()]
+    return [
+        checkpoints,
+        lr_monitor,
+        early_stopping,
+        LogDataModuleCallback(),
+        LogModelCallback(),
+        LogTransformationCallback(),
+    ]
+
 
 def get_logger():
     wandb.login(key=os.environ["WANDB_TOKEN"])
     return WandbLogger(project="exdark")
+
 
 @hydra.main(config_path="../../configs", config_name="config")
 def main(cfg: DictConfig):
@@ -53,6 +64,8 @@ def main(cfg: DictConfig):
         logger=wandb_logger,
     )
     trainer.fit(model, datamodule=exdark_data)
+    trainer.test(ckpt_path="best")
+
 
 if __name__ == "__main__":
     main()
