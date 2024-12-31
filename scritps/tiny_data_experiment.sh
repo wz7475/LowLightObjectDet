46cd7398037fe757dd41d793b4d9fce5d98630c1
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-12-31 13:05:33 +0100

    chore (tiny data experiments): change params in shell script

--- scritps/tiny_data_experiment.sh
+++ scritps/tiny_data_experiment.sh
@@ -2,11 +2,14 @@
 
 echo "num_samples: - mAP" > results_tiny_data.txt
 
-for i in 5 10 50 75 100 200
+for i in 5 10 50 75 100 200 3000
 do
    output=$(python exdark/modeling/train.py \
     "datamodule.limit_to_n_samples=$i" \
-    "datamodule.batch_size=4" \
-    "trainer.max_epochs=5") ;
+    "datamodule.batch_size=4") ;
    echo "$i: $output" >> results_tiny_data.txt ;
 done
+#   output=$(python exdark/modeling/train.py \
+#    "datamodule.limit_to_n_samples=$i" \
+#    "datamodule.batch_size=4" \
+#    "trainer.max_epochs=5") ;
