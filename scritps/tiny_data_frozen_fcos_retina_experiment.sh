a530b03e0b22d1bf782a011b9020ee1ff6045e0d
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-01-02 17:02:21 +0100

    feat (experiment): add script for frozen and not backbone for tiny data retinanet and fcos

---
+++ scritps/tiny_data_frozen_fcos_retina_experiment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "" > results_tiny_data_model.txt
+
+models=("fcos" "retinanet")
+for model in "${models[@]}";
+do
+  for num_samples in 5 10 50 75 100 200 3000
+  do
+    echo "$model $num_samples frozen"
+     output=$(python exdark/modeling/train.py \
+      "datamodule.limit_to_n_samples=$num_samples" \
+      "model=$model" \
+      "model.freeze_backbone=true" \
+      "model.lr_head=0.03" \
+      "datamodule.batch_size=4") ;
+     echo "$num_samples: $output" >> results_tiny_data_model.txt ;
+
+     echo "$model $num_samples not-frozen"
+     output=$(python exdark/modeling/train.py \
+      "datamodule.limit_to_n_samples=$num_samples" \
+      "model=$model" \
+      "model.freeze_backbone=false" \
+      "datamodule.batch_size=4") ;
+     echo "$num_samples: $output" >> results_tiny_data_model.txt ;
+  done
+done
